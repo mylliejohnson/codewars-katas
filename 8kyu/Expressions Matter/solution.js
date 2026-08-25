0fa542b7f70c51737361c2204672891053b484b5
mylliejohnson <51273184+mylliejohnson@users.noreply.github.com>
2026-08-24 21:13:25 -0400

    kata solution

---
+++ 8kyu/Expressions Matter/solution.js
@@ -0,0 +1,12 @@
+const expressionMatter = (a,b,c) => Math.max(a+b+c, a*b*c, a+b*c, a*b+c,(a+b)*c,a*(b+c))
+
+// function expressionMatter(a, b, c) {
+//   return Math.max(
+//   a+b+c,
+//     a*b*c,
+//     a+b*c,   
+//     a*b+c,
+//     (a+b)*c,
+//     a*(b+c)
+//   )
+// } 
