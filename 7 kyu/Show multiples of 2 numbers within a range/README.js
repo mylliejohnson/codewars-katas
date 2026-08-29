d8f1ac0c7cc142ed44418e9ed6858e4c87f9a77b
mylliejohnson <51273184+mylliejohnson@users.noreply.github.com>
2026-08-29 12:07:34 -0400

    Create README for multiples of two numbers
    Add README with problem description and examples for multiples.

---
+++ 7 kyu/Show multiples of 2 numbers within a range/README.js
@@ -0,0 +1,8 @@
+Description:
+Given 3 positive parameters a, b, limit, return all positive numbers that are a multiple of both a and b up to and including limit.
+
+Examples
+1, 5, 15 --> [5, 10, 15]
+3, 5, 15 --> [15]
+3, 5, 40 --> [15, 30]
+2, 4, 40 --> [4, 8, 12, 16, 20, 24, 28, 32, 36, 40]
